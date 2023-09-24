0dd99348e474cd22c586bf8d5bbe94ba4d62613b
Rajavenkateshgurugubelli <120995535+Rajavenkateshgurugubelli@users.noreply.github.com>
2023-09-24 16:30:59 -0500

    Update CalcRunner.java
    format updated

--- src/com/raja/CalcRunner.java
+++ src/com/raja/CalcRunner.java
@@ -8,36 +8,36 @@ public class CalcRunner {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 		for (int i = 0; i < 100; i++) {
-		System.out.println("Enter a number");
-		double a = scan.nextDouble();
-		System.out.println("Enter another number");
-		double b = scan.nextDouble();
-		System.out.println(
-				"Select \n + for addition \n - for substraction \n * for multiplication \n / for division \n % for modulous");
-		char operation = scan.next().charAt(0);
-		Calculator calc = new Calculator(a,b);
-		double result;
-
-		if (operation == '+') {
-			result = calc.add(a, b);
-			System.out.println("Addition of " + a + " and " + b + " is " + result);
-		} else if (operation == '-') {
-			result = calc.sub(a, b);
-			System.out.println("Substraction of " + a + " and " + b + " is " + result);
-		} else if (operation == '*') {
-			result = calc.mul(a, b);
-			System.out.println("Multiply of " + a + " and " + b + " is " + result);
-		} else if (operation == '/') {
-			result = calc.div(a, b);
-			System.out.println("Divison of " + a + " and " + b + " is " + result);
-		} else if (operation == '%') {
-			result = calc.mod(a, b);
-			System.out.println("Modulous of " + a + " and " + b + " is " + result);
-		} else {
-			System.out.println("select vaild operations only !");
+			System.out.println("Enter a number");
+			double a = scan.nextDouble();
+			System.out.println("Enter another number");
+			double b = scan.nextDouble();
 			System.out.println(
-					"Select /n + for addition /n - for substraction /n * for multiplication /n / for division /n % for modulous");
+					"Select \n + for addition \n - for substraction \n * for multiplication \n / for division \n % for modulous");
+			char operation = scan.next().charAt(0);
+			Calculator calc = new Calculator(a, b);
+			double result;
+
+			if (operation == '+') {
+				result = calc.add(a, b);
+				System.out.println("Addition of " + a + " and " + b + " is " + result);
+			} else if (operation == '-') {
+				result = calc.sub(a, b);
+				System.out.println("Substraction of " + a + " and " + b + " is " + result);
+			} else if (operation == '*') {
+				result = calc.mul(a, b);
+				System.out.println("Multiply of " + a + " and " + b + " is " + result);
+			} else if (operation == '/') {
+				result = calc.div(a, b);
+				System.out.println("Divison of " + a + " and " + b + " is " + result);
+			} else if (operation == '%') {
+				result = calc.mod(a, b);
+				System.out.println("Modulous of " + a + " and " + b + " is " + result);
+			} else {
+				System.out.println("select vaild operations only !");
+				System.out.println(
+						"Select /n + for addition /n - for substraction /n * for multiplication /n / for division /n % for modulous");
+			}
 		}
 	}
-}
 }
